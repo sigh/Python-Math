b3fb9f95cf4873a08f5ef0d1f9272dd51692bee8
sigh <sigh.projects@gmail.com>
2010-12-29 18:25:37 +1100

    Add comments to factors.py

--- factors.py
+++ factors.py
@@ -5,7 +5,13 @@ from primes import primes
 """
 Factors
 """
+
 def prime_factors(n):
+    """Calculate prime factors of n
+
+    Prime factors are returned in a dictionary with the primes factors
+    as keys and the count of each factor as the values
+    """
     pf = {}
     
     for p in primes:
@@ -27,12 +33,17 @@ def prime_factors(n):
 
     return pf
 
-def factors(pfd):
-    if type(pfd) != dict: pfd = prime_factors(pfd)
+def factors(n):
+    """Returns factors of n
+
+    n can be a prime factor dictionary as returned by prime_factors(n)
+    """
+
+    if type(n) != dict: n = prime_factors(n)
 
     factors = [1]
     
-    for (pf, count) in pfd.items():
+    for (pf, count) in n.items():
         powers = [pf**i for i in range(1, count+1)]
         factors.extend([p*f for p in powers for f in factors])
 
@@ -42,19 +53,29 @@ def factors(pfd):
 Divisors function
 """
 
-def num_divisiors(pfd):
-    if type(pfd) != dict: pfd = prime_factors(pfd)
+def num_divisiors(n):
+    """Returns the number of divisors of n
 
-    return reduce(mul, (a+1 for a in pfd.values()), 1)
+    n can be a prime factor dictionary as returned by prime_factors(n)
+    """
+    if type(n) != dict: n = prime_factors(n)
 
-def divisor_sum(pfd, x=1):
+    return reduce(mul, (a+1 for a in n.values()), 1)
+
+def divisor_sum(n, x=1):
+    """Returns the sum of divisors of n
+
+    The divisors of will be raised to the power x.
+
+    n can be a prime factor dictionary as returned by prime_factors(n)
+    """
     if x == 0:
-        return num_divisors(pfd)
+        return num_divisors(n)
 
-    if type(pfd) != dict: pfd = prime_factors(pfd)
+    if type(n) != dict: n = prime_factors(n)
 
     result = 1
-    for p,e in pfd.items():
+    for p,e in n.items():
         result *= (p**(x*(e+1)) - 1) // ( p**x - 1)
     return result
 
@@ -63,6 +84,10 @@ Totient
 """
 
 def totient(n):
+    """Returns the number of positive integers (0 < i < n) co-prime to n
+
+    n can be a prime factor dictionary as returned by prime_factors(n)
+    """
     if type(n) == dict:
         return reduce(mul, ((p-1)*p**(e-1) for p,e in n.items()), 1)
     else:
